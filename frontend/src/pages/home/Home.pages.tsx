0147f8066a17ffe74ee0294e41372d6204ff769a
Asam237 <abbasalimokolo@gmail.com>
2021-11-24 18:30:52 +0100

    Page: StreetMap(add button)

--- frontend/src/pages/home/Home.pages.tsx
+++ frontend/src/pages/home/Home.pages.tsx
@@ -5,6 +5,8 @@ import { MyDrawer } from "../../components/drawer/Drawer.component";
 import technique from "../../assets/img/technique.png";
 import { LANGUAGE } from "../../constants/language";
 import { StreetMap } from "../streetMap/StreetMap.page";
+import bolt from "../../assets/img/bolt.png";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   useEffect(() => {
@@ -14,7 +16,20 @@ export const Home = () => {
     <div className="site h-screen">
       <div className="fixed z-10 px-4 md:px-20 pt-5 md:pt-0">
         <div className="container mx-auto">
-          <MyDrawer justify="start" />
+          <div className="flex justify-center items-center">
+            <MyDrawer justify="start" />
+            <div className="site__btn-panne md:ml-20 bg-ind px-2 rounded-3xl mt-3 py-2 ml-4">
+              <Link to="/lists">
+                <p
+                  style={{ fontFamily: " 'Varela Round', sans-serif" }}
+                  className="flex px-4 py-2 text-gray-200"
+                >
+                  <img src={bolt} alt="" className="w-6 h-6 mr-2" />
+                  Panne électrique
+                </p>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
       <main className="site__main">
