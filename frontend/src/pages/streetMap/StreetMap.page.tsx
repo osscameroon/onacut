01a06445584e1140fd92bb3c3e77cec745c7503e
Asam237 <abbasalimokolo@gmail.com>
2021-11-25 22:19:58 +0100

    Component: StyleOpenStreet(Refractoring)

--- frontend/src/pages/streetMap/StreetMap.page.tsx
+++ frontend/src/pages/streetMap/StreetMap.page.tsx
@@ -22,13 +22,14 @@ const SetViewOnClick = (animateRef: any) => {
 };
 
 export const StreetMap = () => {
+  const yaounde: any = [3.826985, 11.495974];
   const animateRef = useRef(false);
 
   return (
     <MapContainer
       className="z-0"
       style={{ height: "100vh" }}
-      center={LATLONG.yaounde}
+      center={yaounde}
       zoom={7}
       zoomControl={false}
       scrollWheelZoom={true}
@@ -37,76 +38,16 @@ export const StreetMap = () => {
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
         url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
       />
-      <CircleMarker
-        center={LATLONG.maroua}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.garoua}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.nagoundere}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.yaounde}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.douala}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.bafoussan}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.bamenda}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.buea}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.ebolowa}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
-      <CircleMarker
-        center={LATLONG.bertoua}
-        pathOptions={{ color: "red" }}
-        radius={2}
-      >
-        <Tooltip>Tooltip for CircleMarker</Tooltip>
-      </CircleMarker>
+      {LATLONG.map((item: any, index: any) => (
+        <CircleMarker
+          key={index}
+          center={item.longlat}
+          pathOptions={{ color: "red" }}
+          radius={5}
+        >
+          <Tooltip>{item.name}</Tooltip>
+        </CircleMarker>
+      ))}
       <LocationMarker />
       <ZoomControl position="topright" zoomInText="🧐" zoomOutText="🗺️" />
       <SetViewOnClick animateRef={animateRef} />
--- frontend/src/scripts/lat_long.ts
+++ frontend/src/scripts/lat_long.ts
@@ -1,16 +1,48 @@
 
-const bamenda: any = [5.914395, 10.129316];
-const bafoussan: any = [5.468774, 10.420834];
-const bertoua: any = [4.558081, 13.662206];
-const buea: any = [4.155587, 9.232463];
-const douala: any = [4.03222, 9.706715];
-const ebolowa: any = [2.891746, 11.15648];
-const garoua: any = [9.278875, 13.394429];
-const maroua: any = [10.588261, 14.350791];
-const nagoundere: any = [7.349664, 13.577051];
-const yaounde: any = [3.826985, 11.495974];
+const bamenda = {
+    name: "Bamenda",
+    longlat: [5.914395, 10.129316]
+}
 
-export const LATLONG = {
+const bafoussan = {
+    name: "Bafoussam",
+    longlat: [5.468774, 10.420834]
+}
+
+const bertoua = {
+    name: "Bertoua",
+    longlat: [4.558081, 13.662206]
+}
+const buea = {
+    name: "Buea",
+    longlat: [4.155587, 9.232463]
+}
+const douala = {
+    name: "Douala",
+    longlat: [4.03222, 9.706715]
+}
+const ebolowa = {
+    name: "Ebolowa",
+    longlat: [2.891746, 11.15648]
+}
+const garoua = {
+    name: "Garoua",
+    longlat: [9.278875, 13.394429]
+}
+const maroua = {
+    name: "Maroua",
+    longlat: [10.588261, 14.350791]
+}
+const nagoundere = {
+    name: "Ngaoundere",
+    longlat: [7.349664, 13.577051]
+}
+const yaounde = {
+    name: "Yaounde",
+    longlat: [3.826985, 11.495974]
+}
+
+export const LATLONG = [
     bamenda,
     bafoussan,
     bertoua,
@@ -21,4 +53,4 @@ export const LATLONG = {
     maroua,
     nagoundere,
     yaounde
-}
+]
