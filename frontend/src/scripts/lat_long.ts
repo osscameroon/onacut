7ad75a4ba834e4422fe44f1527d2c5f784cf9c15
Asam237 <abbasalimokolo@gmail.com>
2021-12-02 05:48:21 +0100

    PopupDisplay: remove console log ALERT on lat_long.ts file

--- frontend/src/scripts/lat_long.ts
+++ frontend/src/scripts/lat_long.ts
@@ -11,7 +11,6 @@ let count_bafoussam: number = 0
 let count_bertoua: number = 0
 let count_bamenda: number = 0
 
-console.log("ALERT:::::", ALERTS)
 
 ALERTS.map((i: any) => {
     if (i.ville === "MAROUA") {
