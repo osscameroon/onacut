aba7ef2e83f93a1fc1386bdf0fba3c49e67c3ba1
Asam237 <abbasalimokolo@gmail.com>
2021-12-07 08:58:28 +0100

    FetchEneoCity: Affichage des villes et quartiers en fonction du Zoom

--- frontend/src/pages/streetMap/StreetMap.page.tsx
+++ frontend/src/pages/streetMap/StreetMap.page.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import L from "leaflet";
 import bolt from "../../assets/img/electricity.png";
 import energy from "../../assets/img/energy.png";
@@ -11,10 +11,23 @@ import {
   ZoomControl,
   Tooltip,
   Marker,
+  useMapEvents,
 } from "react-leaflet";
 import { LocationMarker } from "../../scripts/check_position";
 import { LIST_VILLE } from "../../scripts/list_ville";
 import { LIST_QUARTIER } from "../../scripts/list_quartier";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { zoomLevelState } from "../../atoms/zom_leve";
+
+function MyComponent() {
+  const [zoomLevel, setZoomLevel] = useRecoilState(zoomLevelState); // initial zoom level provided for MapContainer
+  const mapEvents = useMapEvents({
+    zoomend: () => {
+      setZoomLevel(mapEvents.getZoom());
+    },
+  });
+  return null;
+}
 
 const SetViewOnClick = (animateRef: any) => {
   const map = useMapEvent("click", (e) => {
@@ -44,70 +57,96 @@ const lightBolt = L.icon({
 export const StreetMap = () => {
   const yaounde: any = LIST_VILLE[9].longlat;
   const animateRef = useRef(false);
+  const v = useRecoilValue(zoomLevelState);
+  console.log(v);
 
-  return (
-    <MapContainer
-      className="z-0"
-      style={{ height: "100vh" }}
-      center={yaounde}
-      zoom={13}
-      zoomControl={false}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-      />
-      {LIST_VILLE.map((item: any, index: any) => (
-        <Marker position={item.longlat} icon={lightBolt} key={index}>
-          <Tooltip
-            direction="right"
-            className="Tooltip"
-            offset={[2, -40]}
-            permanent
-            opacity={1}
-          >
-            <span
-              style={{
-                fontSize: "8px",
-                fontWeight: "bold",
-                fontFamily: "sans-serif",
-                padding: "5px",
-              }}
-              className="text-xs"
+  if (v > 9) {
+    return (
+      <MapContainer
+        className="z-0"
+        style={{ height: "100vh" }}
+        center={yaounde}
+        zoom={v}
+        zoomControl={false}
+        scrollWheelZoom={true}
+      >
+        <MyComponent />
+        <TileLayer
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+        />
+        {LIST_QUARTIER.map((item: any, index: any) => (
+          <Marker position={item.longlat} icon={energyBolt} key={index}>
+            <Tooltip
+              direction="right"
+              className="Tooltip"
+              offset={[2, -40]}
+              permanent
+              opacity={1}
             >
-              {item.name}
-            </span>
-          </Tooltip>
-        </Marker>
-      ))}
-      {LIST_QUARTIER.map((item: any, index: any) => (
-        <Marker position={item.longlat} icon={energyBolt} key={index}>
-          <Tooltip
-            direction="right"
-            className="Tooltip"
-            offset={[2, -40]}
-            permanent
-            opacity={1}
-          >
-            <span
-              style={{
-                fontSize: "8px",
-                fontWeight: "bold",
-                fontFamily: "sans-serif",
-                padding: "5px",
-              }}
-              className="text-xs"
-            >
-              {item.name}
-            </span>
-          </Tooltip>
-        </Marker>
-      ))}
-      <LocationMarker />
-      <ZoomControl position="bottomright" />
+              <span
+                style={{
+                  fontSize: "8px",
+                  fontWeight: "bold",
+                  fontFamily: "sans-serif",
+                  padding: "5px",
+                }}
+                className="text-xs"
+              >
+                {item.name}
+              </span>
+            </Tooltip>
+          </Marker>
+        ))}
+        <LocationMarker />
+        <ZoomControl position="bottomright" />
 
-      <SetViewOnClick animateRef={animateRef} />
-    </MapContainer>
-  );
+        <SetViewOnClick animateRef={animateRef} />
+      </MapContainer>
+    );
+  } else {
+    return (
+      <MapContainer
+        className="z-0"
+        style={{ height: "100vh" }}
+        center={yaounde}
+        zoom={v}
+        zoomControl={false}
+        scrollWheelZoom={true}
+      >
+        <MyComponent />
+        <TileLayer
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+        />
+        {LIST_VILLE.map((item: any, index: any) => (
+          <Marker position={item.longlat} icon={lightBolt} key={index}>
+            <Tooltip
+              direction="right"
+              className="Tooltip"
+              offset={[2, -40]}
+              permanent
+              opacity={1}
+            >
+              <span
+                style={{
+                  fontSize: "8px",
+                  fontWeight: "bold",
+                  fontFamily: "sans-serif",
+                  padding: "5px",
+                }}
+                className="text-xs"
+              >
+                {item.name}
+              </span>
+            </Tooltip>
+          </Marker>
+        ))}
+        <LocationMarker />
+        <ZoomControl position="bottomright" />
+
+        <SetViewOnClick animateRef={animateRef} />
+      </MapContainer>
+    );
+  }
 };
