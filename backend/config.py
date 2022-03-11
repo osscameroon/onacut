8d10dc1796818784d69b409b6819764339b8af32
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-03-11 02:42:59 +0100

    update config file by adding flask-apispec congig

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,8 @@
 import os
 
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
 class BaseConfig(object):
     DEBUG = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,6 +13,14 @@ class DevConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BaseConfig.BASE_DIR, "onacut.data")
     SQLALCHEMY_COMMIT_ON_TREARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATION = True
+    APISPEC_SPEC = APISpec(
+        title="OnaCut API",
+        version="v1",
+        plugins=[MarshmallowPlugin()],
+        openapi_version="2.0.0"
+    )
+    APISPEC_SWAGGER_URL = "/swagger/"
+    APISPEC_SWAGGER_UI_URL = "/swagger-ui/"
 
 
 class ProdConfig(BaseConfig):
