09dd9d9261083662dbf0ba456744d1fcd143a3b3
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-03-13 10:34:43 +0100

    add endpoint and doc to app

--- backend/onacut/api/__init__.py
+++ backend/onacut/api/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_apispec.extension import FlaskApiSpec
 from .cities.controllers import CitiesApi
 from .alerts.controllers import AlertsApi
+from .regions.controllers import RegionsApi
 
 
 api = Api()
@@ -11,7 +12,9 @@ doc = FlaskApiSpec()
 def create_module(app, **kwargs):
     api.add_resource(CitiesApi, "/api/cities")
     api.add_resource(AlertsApi, "/api/alerts")
+    api.add_resource(RegionsApi, "/api/regions")
     api.init_app(app)
     doc.init_app(app)
     doc.register(AlertsApi)
     doc.register(CitiesApi)
+    doc.register(RegionsApi)
---
+++ backend/onacut/api/regions/controllers.py
@@ -0,0 +1,26 @@
+import datetime
+
+from onacut import db, app
+from onacut.models import City
+from flask_restful import Resource
+from flask_apispec import marshal_with, doc, use_kwargs
+from flask_apispec.views import MethodResource
+from .parsers import city_get_parser, CityGetParser
+from .fields import CityGetResponseSchema
+
+class CitiesApi(MethodResource, Resource):
+    @doc(description='GET all Cities.', tags=['Cities'])
+    @use_kwargs(CityGetParser, location=("json"))
+    @marshal_with(CityGetResponseSchema(many=True))
+    def get(self):
+        args = city_get_parser.parse_args()
+        city_id = args["id"]
+        if city_id:
+            city = City.query.get(city_id)
+            if not city:
+                abort(404)
+            return [city], 200
+        
+        cities = City.query.all()
+        
+        return cities, 200
