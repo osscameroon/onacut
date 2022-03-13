250a6c9854a9fc1895e906864d1af7aab6de84d3
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-03-13 10:36:28 +0100

    define regions endpoint req params

--- backend/onacut/api/regions/parsers.py
+++ backend/onacut/api/regions/parsers.py
@@ -1,13 +1,13 @@
 from flask_restful import reqparse
 from marshmallow import Schema, fields
 
-city_get_parser = reqparse.RequestParser()
-city_get_parser.add_argument(
+region_get_parser = reqparse.RequestParser()
+region_get_parser.add_argument(
     "id",
     type=int,
     location=["args", "values", "form", "json", "headers"]
 )
 
 
-class CityGetParser(Schema):
+class RegionGetParser(Schema):
     id = fields.Integer(required=False, description="Id of the aler")
