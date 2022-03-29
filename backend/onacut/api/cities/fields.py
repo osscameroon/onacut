13b7652fd112dd5f5266cc87517e64b1191f6184
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-03-29 04:55:07 +0200

    update models

--- backend/onacut/api/cities/fields.py
+++ backend/onacut/api/cities/fields.py
@@ -6,4 +6,6 @@ from ..locations.fields import LocationGetResponseSchema
 class CityGetResponseSchema(Schema):
     id = fields.Integer()
     name = fields.String()
+    longitude = fields.Float()
+    lattitude = fields.Float()
     locations = fields.List(fields.Nested(LocationGetResponseSchema))
