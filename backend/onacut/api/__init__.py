71b4aec504de2cec5a851d482e5eaa3b017d1cff
sanix-darker <s4nixd@gmail.com>
2022-07-05 10:54:01 +0200

    feat: added a default home endpoint for api or /

--- backend/onacut/api/__init__.py
+++ backend/onacut/api/__init__.py
@@ -1,6 +1,7 @@
 from flask_apispec.extension import FlaskApiSpec
 from flask_restful import Api
 
+from .home.controllers import Home
 from .alerts.controllers import AlertsApi
 from .cities.controllers import CitiesApi
 from .district.controllers import DistrictsApi
@@ -11,11 +12,13 @@ doc = FlaskApiSpec()
 
 
 def create_module(app, **kwargs):
+    api.add_resource(Home, "/", "/api")
     api.add_resource(CitiesApi, "/api/cities")
     api.add_resource(DistrictsApi, "/api/districts")
     api.add_resource(AlertsApi, "/api/alerts")
     api.add_resource(RegionsApi, "/api/regions")
     api.init_app(app)
+
     doc.init_app(app)
     doc.register(AlertsApi)
     doc.register(CitiesApi)
