ecbf7757cd241e7a096004f61986163071b0b050
darker <s4nixd@gmail.com>
2022-07-08 07:27:51 +0200

    fix: get all cities if there is a badRequest raised (#100)
    * fix: get all cities if there is a badRequest raised
    
    * doc: added a more eplicit comment

--- backend/onacut/api/cities/controllers.py
+++ backend/onacut/api/cities/controllers.py
@@ -2,8 +2,8 @@ from flask import abort
 from flask_apispec import doc, marshal_with, use_kwargs
 from flask_apispec.views import MethodResource
 from flask_restful import Resource
+from werkzeug.exceptions import BadRequest
 
-import werkzeug
 from onacut.models import City
 
 from .fields import CityGetResponseSchema
@@ -15,6 +15,7 @@ class CitiesApi(MethodResource, Resource):
     @use_kwargs(CityGetParser, location=("json"))
     @marshal_with(CityGetResponseSchema(many=True))
     def get(self):
+        cities = []
         try:
             args = city_get_parser.parse_args()
             city_id = args["id"]
@@ -24,9 +25,11 @@ class CitiesApi(MethodResource, Resource):
                     abort(404)
                 return [city], 200
 
+            return cities, 200
+
+        except BadRequest:
+            # that's means no arguments got passed or a weird argument was
+            # passed so that the json parsing crashed
             cities = City.query.all()
-        except werkzeug.exceptions.BadRequest:
-            # that's means no arguments got passed
-            cities = []
 
         return cities, 200
