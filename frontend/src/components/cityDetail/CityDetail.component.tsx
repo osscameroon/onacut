b23395e2bd5e575623442f8306397b86925596d1
godsakani <nyonggodwill11@gmail.com>
2022-07-21 22:17:09 +0100

    feat: added latitude and longitude to button

--- frontend/src/components/cityDetail/CityDetail.component.tsx
+++ frontend/src/components/cityDetail/CityDetail.component.tsx
@@ -8,6 +8,7 @@ import city from "../../assets/img/city.png";
 import home from "../../assets/img/home.png"
 import { LANGUAGE } from "../../constants/language";
 import { Link } from "react-router-dom";
+import StreetMap from "../../pages/streetMap/StreetMap.page";
 
 export const CityDetail = (props: any) => {
         return (
@@ -59,7 +60,7 @@ export const CityDetail = (props: any) => {
             </div>
             <div className="mt-4 py-2 bg-ind rounded-3xl flex items-center justify-center "onClick={props.myClick}>
               <Link onClick={props.myClick}
-                        to={`/?lat=${props.lattitude}&long=${props.longitude}`}>
+                        to={`/?lat=${props.lat}&long=${props.long}`}>
               <button className="px-4 text-white">
                 <p className="flex px-2 py-1" >
                     <img src={home} alt={LANGUAGE.button.location} className="w-5 h-auto flex items-center justify-center"/>
