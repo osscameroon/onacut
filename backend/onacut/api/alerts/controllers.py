2b507f02681a878aa2a3e3e484e2f2798943acb5
darker <s4nixd@gmail.com>
2022-07-31 22:55:33 +0200

    fix: bad request error on json parser (#116)

--- backend/onacut/api/alerts/controllers.py
+++ backend/onacut/api/alerts/controllers.py
@@ -1,13 +1,12 @@
-from flask import abort
+from flask import request
 from flask_apispec import doc, marshal_with, use_kwargs
 from flask_apispec.views import MethodResource
 from flask_restful import Resource
-from werkzeug.exceptions import BadRequest
 
 from onacut.models import Alert, City, District, Region
 
 from .fields import AlertGetResponseSchema
-from .parsers import AlertGetParser, alert_get_parser
+from .parsers import AlertGetParser
 
 
 class AlertsApi(MethodResource, Resource):
@@ -15,48 +14,39 @@ class AlertsApi(MethodResource, Resource):
     @use_kwargs(AlertGetParser, location=("json"))
     @marshal_with(AlertGetResponseSchema(many=True))
     def get(self):
-        try:
-            args = alert_get_parser.parse_args()
 
-            alert_id = args.get("id", None)
-            region_name = args.get("region", None)
-            city_name = args.get("city", None)
-            district_name = args.get("district", None)
+        alert_id = request.args.get("id", None)
+        region_name = request.args.get("region", None)
+        city_name = request.args.get("city", None)
+        district_name = request.args.get("district", None)
 
-            alerts = Alert.query
+        alerts = Alert.query
 
-            if alert_id:
-                alert = alerts.get(alert_id)
-                if not alert:
-                    abort(404)
-                return [alert], 200
+        if alert_id:
+            alert = alerts.get(alert_id)
+            if not alert:
+                return [], 200
+            return [alert], 200
 
-            if region_name:
-                region = Region.query.filter_by(name=region_name.lower()).first()
-                if not region:
-                    abort(404)
+        if region_name:
+            region = Region.query.filter_by(name=region_name.lower()).first()
+            if not region:
+                return [], 200
 
-                alerts = alerts.filter_by(region_id=region.id)
+            alerts = alerts.filter_by(region_id=region.id)
 
-            if city_name:
-                city = City.query.filter_by(name=city_name.lower()).first()
-                if not city:
-                    abort(404)
+        if city_name:
+            city = City.query.filter_by(name=city_name.lower()).first()
+            if not city:
+                return [], 200
 
-                alerts = alerts.filter_by(city_id=city.id)
+            alerts = alerts.filter_by(city_id=city.id)
 
-            if district_name:
-                district = District.query.filter_by(name=district_name.lower()).first()
-                if not district:
-                    abort(404)
+        if district_name:
+            district = District.query.filter_by(name=district_name.lower()).first()
+            if not district:
+                return [], 200
 
-                alerts = alerts.filter_by(district_id=district.id)
-
-            return alerts.all(), 200
-        except BadRequest:
-            # FIXME , this is a temp solution 9we should not return results
-            # on bad request +that's means no arguments got passed or a weird argument was
-            # passed so that the json parsing crashed
-            alerts = Alert.query
+            alerts = alerts.filter_by(district_id=district.id)
 
         return alerts.all(), 200
--- backend/onacut/api/cities/controllers.py
+++ backend/onacut/api/cities/controllers.py
@@ -1,13 +1,12 @@
-from flask import abort
+from flask import request
 from flask_apispec import doc, marshal_with, use_kwargs
 from flask_apispec.views import MethodResource
 from flask_restful import Resource
-from werkzeug.exceptions import BadRequest
 
 from onacut.models import City
 
 from .fields import CityGetResponseSchema
-from .parsers import CityGetParser, city_get_parser
+from .parsers import CityGetParser
 
 
 class CitiesApi(MethodResource, Resource):
@@ -16,22 +15,13 @@ class CitiesApi(MethodResource, Resource):
     @marshal_with(CityGetResponseSchema(many=True))
     def get(self):
         cities = []
-        try:
-            args = city_get_parser.parse_args()
-            city_id = args.get("id", None)
-            if city_id:
-                city = City.query.get(city_id)
-                if not city:
-                    abort(404)
-                return [city], 200
+        city_id = request.args.get("id", None)
+        if city_id:
+            city = City.query.get(city_id)
+            if not city:
+                return [], 200
 
-            cities = City.query.all()
-            return cities, 200
+            return [city], 200
 
-        except BadRequest:
-            # FIXME , this is a temp solution 9we should not return results
-            # on bad request +that's means no arguments got passed or a weird argument was
-            # passed so that the json parsing crashed
-            cities = City.query.all()
-
+        cities = City.query.all()
         return cities, 200
