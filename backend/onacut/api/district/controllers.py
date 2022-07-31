eeb90246e2b614443f569a110414bdc1655b4bf7
sanix-darker <s4nixd@gmail.com>
2022-07-31 22:53:50 +0200

    fix: bad request error on json parser

--- backend/onacut/api/district/controllers.py
+++ backend/onacut/api/district/controllers.py
@@ -1,4 +1,4 @@
-from flask import abort
+from flask import request
 from flask_apispec import doc, marshal_with, use_kwargs
 from flask_apispec.views import MethodResource
 from flask_restful import Resource
@@ -6,7 +6,7 @@ from flask_restful import Resource
 from onacut.models import District
 
 from .fields import DistrictGetResponseSchema
-from .parsers import DistrictGetParser, district_get_parser
+from .parsers import DistrictGetParser
 
 
 class DistrictsApi(MethodResource, Resource):
@@ -14,12 +14,11 @@ class DistrictsApi(MethodResource, Resource):
     @use_kwargs(DistrictGetParser, location=("json"))
     @marshal_with(DistrictGetResponseSchema(many=True))
     def get(self):
-        args = district_get_parser.parse_args()
-        district_id = args["id"]
+        district_id = request.args.get("id", None)
         if district_id:
             district = District.query.get(district_id)
             if not district:
-                abort(404)
+                return [], 200
             return [district], 200
 
         districts = District.query.all()
