a9dc3fe0c909d72300f76d6dbbfcfe2e818ad6cc
godsakani <nyonggodwill11@gmail.com>
2022-09-06 20:54:25 +0100

    feat: added languange translation files

--- frontend/src/pages/advices/Advices.tsx
+++ frontend/src/pages/advices/Advices.tsx
@@ -3,9 +3,10 @@ import { LINKS } from "../../assets/data/Links.data";
 import { Accordion } from "../../components/accordion/Accordion.component";
 import { MyDrawer } from "../../components/drawer/Drawer.component";
 import { MyText } from "../../components/myText/MyText.component";
-import { LANGUAGE } from "../../constants/language";
+import { useTranslation } from "react-i18next";
 
 const Advices = () => {
+    const {t} = useTranslation();
     return (
         <div className="site__conseils bg-cover w-auto h-screen">
             <div className="px-4 md:px-20 pt-5 md:pt-0">
@@ -14,19 +15,19 @@ const Advices = () => {
                     <main className="site__main pt-8 px-4 md:px-40">
                         <MyText
                             variant="title"
-                            myText={LANGUAGE.info.title}
+                            myText={t('title1')}
                             myTextColor="text-white"
                         />
                         <div className="w-full md:w-3/4 py-4">
                             <MyText
                                 variant="normal"
-                                myText={LANGUAGE.info.content}
+                                myText={t('contents')}
                                 myTextColor="text-white"
                             />
                         </div>
                         <MyText
                             variant="normal"
-                            myText={LANGUAGE.info.link}
+                            myText={t('link')}
                             myTextColor="text-ind"
                         />
                         {LINKS.map((i: any) => (
