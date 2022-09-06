5707a2e8919e243f0949a8335ee8077c31e07b3f
godsakani <nyonggodwill11@gmail.com>
2022-09-06 20:46:18 +0100

    file: Added file for languageSelect

--- frontend/src/languageSelect.js
+++ frontend/src/languageSelect.js
@@ -1,71 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import i18next from "i18next";
 
-import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
-import Button from "@material-ui/core/Button";
-import Popover from "@material-ui/core/Popover";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListSubheader from "@material-ui/core/ListSubheader";
+const LanguageSelector = () => {
 
-const languageMap = {
-  en: { label: "English", dir: "ltr", active: true },
-  fr: { label: "Français", dir: "ltr", active: false }
-};
-
-const LanguageSelect = () => {
-  // eslint-disable-next-line no-undef
-  const selected = localStorage.getItem("i18nextLng") || "en";
-  const { t } = useTranslation();
-
-  const [menuAnchor, setMenuAnchor] = React.useState(null);
-  React.useEffect(() => {
-    // eslint-disable-next-line no-undef
-    document.body.dir = languageMap[selected].dir;
-  }, [menuAnchor, selected]);
-
+  const { i18n } = useTranslation();
+  
+  const changeLanguage = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
   return (
-    <div className="d-flex justify-content-end align-items-center language-select-root">
-      <Button onClick={({ currentTarget }) => setMenuAnchor(currentTarget)}>
-        {languageMap[selected].label}
-        <ArrowDropDown fontSize="small" />
-      </Button>
-      <Popover
-        open={!!menuAnchor}
-        anchorEl={menuAnchor}
-        onClose={() => setMenuAnchor(null)}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right"
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right"
-        }}
-      >
-        <div>
-          <List>
-            <ListSubheader>{t("select_language")}</ListSubheader>
-            {Object.keys(languageMap)?.map(item => (
-              <ListItem
-                button
-                key={item}
-                onClick={() => {
-                  i18next.changeLanguage(item);
-                  setMenuAnchor(null);
-                }}
-              >
-                {languageMap[item].label}
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      </Popover>
+    <div onChange={changeLanguage}>
+      <input type="radio" value="en" name="language" defaultChecked /> English
+      <input type="radio" value="fr" name="language" /> French
     </div>
   );
 };
-
-export default LanguageSelect;
-
-// { [key: string]: any }
+export default LanguageSelector;
