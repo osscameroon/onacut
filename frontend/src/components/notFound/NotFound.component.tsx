ea921ea4aee3f04007dc9348b27bac3a5dfbdfdf
godsakani <nyonggodwill11@gmail.com>
2022-09-06 20:50:52 +0100

    feat: added translation to files

--- frontend/src/components/notFound/NotFound.component.tsx
+++ frontend/src/components/notFound/NotFound.component.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import error from "../../assets/img/error.png";
-import { LANGUAGE } from "../../constants/language";
 import { MyText } from "../myText/MyText.component";
+import { useTranslation } from "react-i18next";
 
 export const NotFound = () => {
+    const { t } = useTranslation();
     return (
         <div className="site__notFound flex justify-center items-center h-screen flex-col">
             <img src={error} alt="error" className="h-1/4 w-auto" />
             <div className="w-full md:w-1/3 py-6">
                 <MyText
                     variant="title"
-                    myText={LANGUAGE.alert.notavailable}
+                    myText={t("notAvailable")}
                     myTextColor="text-ind"
                     myTextAlign="text-center"
                 />
                 <MyText
                     variant="normal"
-                    myText={LANGUAGE.alert.warning}
+                    myText={t("warning")}
                     myTextColor="text-ind"
                     myTextAlign="text-center"
                 />
@@ -28,7 +29,7 @@ export const NotFound = () => {
                         className="text-gray-300"
                         style={{ fontFamily: " 'Varela Round', sans-serif" }}
                     >
-                        {LANGUAGE.notfound.homePage}
+                        {t("homePage")}
                     </p>
                 </Link>
             </div>
