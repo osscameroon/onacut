38de3214d9998b9d70c5145978879f515b5257d1
Steve Yonkeu <yokwejuste@yahoo.com>
2022-09-09 23:36:32 +0100

    feat: post anf put methods added

--- backend/onacut/api/alerts/controllers.py
+++ backend/onacut/api/alerts/controllers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import request
 from flask_apispec import doc, marshal_with, use_kwargs
 from flask_apispec.views import MethodResource
@@ -7,6 +9,7 @@ from onacut.models import Alert, City, District, Region
 
 from .fields import AlertGetResponseSchema
 from .parsers import AlertGetParser
+from onacut import db
 
 
 class AlertsApi(MethodResource, Resource):
@@ -16,7 +19,7 @@ class AlertsApi(MethodResource, Resource):
     def get(self):
 
         alert_id = request.args.get("id", None)
-        region_name = request.args.get("region", None)
+        region_name = request.args.get("region", None)0
         city_name = request.args.get("city", None)
         district_name = request.args.get("district", None)
 
@@ -50,3 +53,56 @@ class AlertsApi(MethodResource, Resource):
             alerts = alerts.filter_by(district_id=district.id)
 
         return alerts.all(), 200
+
+    def post(self):
+        alert = Alert(
+            region_id=request.json['region_id'],
+            city_id=request.json['city_id'],
+            district_id=request.json['district_id'],
+            type=request.json['type'],
+            observations=request.json['observations'],
+            lattitude=request.json['latitude'],
+            longitude=request.json['longitude'],
+            date=datetime.datetime.now().date(),
+            begin_time=datetime.datetime.now().time(),
+            end_time=datetime.datetime.now().time()
+        )
+
+        db.session.add(alert)
+        db.session.commit()
+        return {
+            "message": "Success"
+        }
+
+    def put(self, alert_id):
+        alerts = Alert.query.all()
+        if alert_id not in alerts:
+            return {
+                "message": f"Error the alert with ID { alert_id } doesn't exit"
+            }
+        alert = alerts[alert_id]
+        if 'observations' in request.json:
+            alert['observations'] = request.json['observations']
+        if 'type' in request.json:
+            alert['type'] = request.json['type']
+        if 'date' in request.json:
+            alert['date'] = request.json['date']
+        if 'begin_time' in request.json:
+            alert['begin_time'] = request.json['begin_time']
+        if 'end_time' in request.json:
+            alert['end_time'] = request.json['end_time']
+        if 'longitude' in request.json:
+            alert['longitude'] = request.json['longitude']
+        if 'lattitude' in request.json:
+            alert['lattitude'] = request.json['lattitude']
+        if 'region_id' in request.json:
+            alert['region_id'] = request.json['region_id']
+        if 'city_id' in request.json:
+            alert['city_id'] = request.json['city_id']
+        if 'district_id' in request.json:
+            alert['district_id'] = request.json['district_id']
+        return {
+            200: {
+                "message": "Successfully edited the alert"
+            }
+        }
