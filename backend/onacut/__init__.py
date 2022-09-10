3549743bc7256b1a5a283cb8fa06d85b38bc892c
Steve Yonkeu <yokwejuste@yahoo.com>
2022-09-10 08:44:45 +0100

    feat: code cleanup

--- backend/onacut/__init__.py
+++ backend/onacut/__init__.py
@@ -18,8 +18,6 @@ else:
     app.config.from_object(DevConfig)
 
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 
