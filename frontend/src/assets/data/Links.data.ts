642feed2cdda053283dea8e4ef6918b2a1855c58
godsakani <nyonggodwill11@gmail.com>
2022-09-23 06:42:48 +0100

    feat: added translation for links data item

--- frontend/src/assets/data/Links.data.ts
+++ frontend/src/assets/data/Links.data.ts
@@ -1,20 +1,22 @@
+import i18n from "../../i18n";
+
 export const LINKS = [
   {
-    title: "Que faire en cas de panne de courant ",
+    title: i18n.t("adviceTitle2"),
     content: [
-      "S'agit-il d'une panne de courant qui n'affecte que les appareils individuels ? Vos voisins ont également eu une coupure de courant ? Il est possible qu'un seul fusible ait grillé et que vous puissiez résoudre le problème vous-même.",
+      i18n.t("adviceContent"),
     ]
   },
   {
-    title: "Comportement après une panne de courant ",
+    title:i18n.t( "adviceTitle3"),
     content: [
-      "Dès que le courant est rétabli, dans la plupart des cas, il y a d'abord un soulagement. Mais il y a certaines choses que vous devez garder à l'esprit même après une panne de courant.",
+      i18n.t("adviceContent1"),
     ]
   },
   {
-    title: "Raisons d'une panne de courant ",
+    title: i18n.t("adviceTitle4"),
     content: [
-      "En cas de panne de courant, le plus important est de rétablir l'alimentation. Pour la plupart des personnes touchées par une panne de courant, cela peut être réalisé au moyen de mesures de commutation dans le réseau électrique. L'opérateur du réseau ne connaît souvent la raison exacte de la panne de courant qu'un certain temps plus tard.",
+      i18n.t("adviceContent2"),
     ]
   }
 ]
