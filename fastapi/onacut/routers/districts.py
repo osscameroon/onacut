9a3857016887277190c2976003ea9269bdad4e18
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-10 17:24:10 +0200

    prepare /districts methods and DistrictsSchema

---
+++ fastapi/onacut/routers/districts.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..schemas.districts import District as DistrictSchema 
+
+router = APIRouter(
+    prefix="/districts",
+    tags=["districts"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/")
+async def read_districts():
+    return []
+
+
+@router.get("/{district_id}")
+async def get_district(district_id: int):
+    return {}
+
+
+@router.post(
+    "/",
+    tags=["districts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def create_district(district: DistrictSchema):
+    return {}
+
+
+@router.put(
+    "/{district_id}",
+    tags=["districts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_district(district_id: int, district: DistrictSchema):
+    return {}
+
+
+@router.delete(
+    "/{district_id}",
+    tags=["districts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def delete_district(district_id: int):
+    return {}
---
+++ fastapi/onacut/schemas/districts.py
@@ -0,0 +1,9 @@
+from typing import List, Union
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class District(BaseModel):
+    id: int
+    name: str
+    city_id: int
