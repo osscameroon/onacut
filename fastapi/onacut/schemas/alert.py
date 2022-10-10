783a71758a2db317ce66d7f0832ba46726e44830
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 00:35:52 +0200

    change the alert type to choice over the possible values

--- fastapi/onacut/schemas/alert.py
+++ fastapi/onacut/schemas/alert.py
@@ -1,12 +1,13 @@
 from typing import List, Union
 from pydantic import BaseModel
 from datetime import datetime
+from pydantic_choices import choice
 
 
 class Alert(BaseModel):
     id: int
     observations: str
-    type: str
+    type: choice(["electricity", "water", "internet"])
     date: datetime
     begin_time: datetime
     end_time: datetime
