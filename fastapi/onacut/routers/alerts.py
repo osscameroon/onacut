fb5f4b83c05d203672ef2bcc275e9cf6172f7e3a
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-10 17:00:32 +0200

    prepare /alerts methods

---
+++ fastapi/onacut/routers/alerts.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..schemas.alert import Alert as AlertSchema 
+
+router = APIRouter(
+    prefix="/alerts",
+    tags=["alerts"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/")
+async def read_alerts():
+    return []
+
+
+@router.get("/{alert_id}")
+async def get_alert(alert_id: int):
+    return {}
+
+
+@router.post(
+    "/",
+    tags=["alerts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def create_alert(alert: AlertSchema):
+    return {}
+
+
+@router.put(
+    "/{alert_id}",
+    tags=["alerts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_alert(alert_id: int, alert: AlertSchema):
+    return {}
+
+
+@router.delete(
+    "/{alert_id}",
+    tags=["alerts"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def delete_alert(alert_id: int):
+    return {}
