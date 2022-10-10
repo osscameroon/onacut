ae04ecb805323e485ee48d77e8fe67d4422bc067
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-10 17:25:42 +0200

    fix lint

--- fastapi/onacut/main.py
+++ fastapi/onacut/main.py
@@ -1,14 +1,18 @@
 from fastapi import Depends, FastAPI
-
-# from .dependencies import get_query_token, get_token_header
 from .routers.cities import router as cities_router
+from .routers.alerts import router as alerts_router
+from .routers.districts import router as districts_router
+from .routers.regions import router as regions_router
 
-# app = FastAPI(dependencies=[Depends(get_query_token)])
+
 app = FastAPI()
-
 app.include_router(cities_router)
+app.include_router(alerts_router)
+app.include_router(districts_router)
+app.include_router(regions_router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
