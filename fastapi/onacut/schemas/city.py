8c70d3a0f3511de3cf1f6a44bb604c53463725c4
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-10 16:43:18 +0200

    writte the City's Schema

---
+++ fastapi/onacut/schemas/city.py
@@ -0,0 +1,10 @@
+from typing import List, Union
+from pydantic import BaseModel
+
+
+class City(BaseModel):
+    id: int
+    name: str
+    region_id: int
+    longitude: float
+    lattitude: float
