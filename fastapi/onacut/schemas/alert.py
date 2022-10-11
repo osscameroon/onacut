ccaf147e82c98669b755621f780c2793c59b2b3d
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 21:11:59 +0200

    create Alert's Schemas for all methods

--- fastapi/onacut/schemas/alert.py
+++ fastapi/onacut/schemas/alert.py
@@ -1,18 +1,34 @@
-from typing import List, Union
+from typing import Union
 from pydantic import BaseModel
 from datetime import datetime
 from pydantic_choices import choice
 
 
-class Alert(BaseModel):
-    id: int
+class AlertBase(BaseModel):
     observations: str
     type: choice(["electricity", "water", "internet"])
     date: datetime
     begin_time: datetime
-    end_time: datetime
     region_id: int
     longitude: float
     lattitude: float
     city_id: int
     district_id: int
+
+
+class AlertCreate(AlertBase):
+    longitude: Union[float, None] = None
+    lattitude: Union[float, None] = None
+    end_time: Union[datetime, None] = None
+
+
+class AlertUpdate(AlertBase):
+    id: int
+    longitude: Union[float, None] = None
+    lattitude: Union[float, None] = None
+    end_time: Union[datetime, None] = None
+
+
+class Alert(AlertBase):
+    id: int
+    end_time: Union[datetime, None] = None
