4728bcb59ab6f71d342ad9b9b365d040f14b49ef
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 21:12:50 +0200

    create Region's Schemas for all methods

--- fastapi/onacut/schemas/region.py
+++ fastapi/onacut/schemas/region.py
@@ -2,8 +2,26 @@ from typing import List, Union
 from pydantic import BaseModel
 from datetime import datetime
 
+from .city import City as CitySchema
+from .alert import Alert as AlertSchema
 
-class Region(BaseModel):
+
+class RegionBase(BaseModel):
     id: int
     name: str
     total_alerts: int
+
+
+class RegionCreate(RegionBase):
+    pass
+
+
+class RegionUpdate(RegionBase):
+    id: int
+
+
+class Region(RegionBase):
+    id: int
+    total_alerts: Union[int, None] = None
+    cities: Union[List[CitySchema], None] = None
+    alerts: Union[List[AlertSchema], None] = None
