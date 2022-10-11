f437cc1aa4003d995dd6640c821a0827572c5435
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 21:12:14 +0200

    create City's Schemas for all methods

--- fastapi/onacut/schemas/city.py
+++ fastapi/onacut/schemas/city.py
@@ -1,11 +1,30 @@
 from typing import List, Union
 from pydantic import BaseModel
 
+from .alert import Alert as AlertSchema
+from .districts import District as DistrictSchema
 
-class City(BaseModel):
-    id: int
+
+class CityBase(BaseModel):
     name: str
     region_id: int
     longitude: float
     lattitude: float
-    total_alerts: int
+    
+
+
+class CityCreate(CityBase):
+    pass
+
+
+class CityUpdate(CityBase):
+    id: int
+    longitude: Union[float, None] = None
+    lattitude: Union[float, None] = None
+
+
+class City(CityBase):
+    id: int
+    total_alerts: Union[int, None] = None
+    alerts: Union[List[AlertSchema], None] = None
+    districts: Union[List[DistrictSchema], None] = None
