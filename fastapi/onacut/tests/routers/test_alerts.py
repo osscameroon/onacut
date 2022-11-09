4b47bd93b8226c2322d37970f2fae730e80387cd
sanix-darker <s4nixd@gmail.com>
2022-11-09 14:14:35 +0100

    fix: update/fix test alert to play with our own test database

--- fastapi/onacut/tests/routers/test_alerts.py
+++ fastapi/onacut/tests/routers/test_alerts.py
@@ -1,6 +1,10 @@
+from uuid import uuid4
+
 import pytest
 
 from onacut.tests import client
+
+_BAD_ID = 0000000
 
 
 @pytest.fixture
@@ -9,13 +13,41 @@ def metadata():
         "observations": "Test Add Alert",
         "date": "2022-10-30",
         "begin_time": "18:00:00",
-        "region_id": 4,
         "longitude": 11.124576,
         "lattitude": 4.012475,
-        "city_id": 1,
-        "district_id": 1,
         "end_time": "20:30:00",
     }
+
+
+@pytest.fixture
+def region_id() -> int:
+    # We test the post of a region directly here
+    response_region = client.post("/regions/", json={"name": str(uuid4())})
+    return response_region.json()["id"]
+
+
+@pytest.fixture
+def city_id(region_id: int) -> int:
+    # We test the post of a city directly here
+    response_city = client.post(
+        "/cities/",
+        json={
+            "name": str(uuid4()),
+            "longitude": 11.124576,
+            "lattitude": 4.012475,
+            "region_id": region_id,
+        },
+    )
+    return response_city.json()["id"]
+
+
+@pytest.fixture
+def district_id(city_id: int) -> int:
+    # We test the post of a district directly here
+    response_district = client.post(
+        "/districts/", json={"name": str(uuid4()), "city_id": city_id}
+    )
+    return response_district.json()["id"]
 
 
 def test_read_all_alerts():
@@ -23,8 +55,19 @@ def test_read_all_alerts():
     assert response.status_code == 200
 
 
-def test_add_alert(metadata: dict):
-    alert = {"type": "electricity", **metadata}
+def test_add_alert(
+    metadata: dict, region_id: int, city_id: int, district_id: int
+) -> None:
+    """To add an alert, we need to have a region,a city and a district"""
+    alert = {
+        **metadata,
+        **{
+            "type": "electricity",
+            "region_id": region_id,
+            "city_id": city_id,
+            "district_id": district_id,
+        },
+    }
 
     response = client.post("/alerts/", json=alert)
 
@@ -34,33 +77,43 @@ def test_add_alert(metadata: dict):
     assert item == alert
 
 
-def test_add_alert_bad_region(metadata: dict):
-    alert = {"type": "water", **metadata}
+def test_add_alert_bad_region_city_district(
+    metadata: dict, region_id: str, city_id: int, district_id: int
+) -> None:
+    for region, city, district in [
+        (_BAD_ID, city_id, district_id),
+        (region_id, _BAD_ID, district_id),
+        (_BAD_ID, city_id, district_id),
+        (region_id, city_id, _BAD_ID),
+    ]:
+        alert = {
+            "type": "water",
+            **metadata,
+            **{"region_id": region, "city_id": city, "district_id": district},
+        }
 
+        response = client.post("/alerts/", json=alert)
+
+        assert response.status_code == 400
+        bad_element = (
+            "region"
+            if region == _BAD_ID
+            else ("city" if city == _BAD_ID else "district")
+        )
+        assert response.json() == {"detail": f"Bad {bad_element}'s id!"}
+
+
+def test_delete_alert(metadata: dict, region_id: int, city_id: int, district_id: int):
+    alert = {
+        **metadata,
+        **{
+            "type": "electricity",
+            "region_id": region_id,
+            "city_id": city_id,
+            "district_id": district_id,
+        },
+    }
     response = client.post("/alerts/", json=alert)
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Bad region's id!"}
 
-
-def test_add_alert_bad_city(metadata: dict):
-    alert = {"type": "water", **metadata}
-
-    response = client.post("/alerts/", json=alert)
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Bad city's id!"}
-
-
-def test_add_alert_bad_district(metadata: dict):
-    alert = {"type": "water", **metadata}
-
-    response = client.post("/alerts/", json=alert)
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Bad district's id!"}
-
-
-def test_delete_alert():
-    response = client.get("/alerts/")
-    assert len(response.json())
-    res = client.delete(f"/alerts/{len(response.json()) - 1}")
+    res = client.delete(f"/alerts/{response.json()['id']}")
     assert res.status_code == 200
-    assert res.json() is None
