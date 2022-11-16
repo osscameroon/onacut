9d8f33338737b01f42c4a8a3279c72523b460a75
#Steven <45762805+mouafus@users.noreply.github.com>
2022-11-16 10:18:14 +0100

    fix 404 redirect error

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+export const config = {
+    apiUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api'
+}
